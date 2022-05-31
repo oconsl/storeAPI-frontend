57f3f48a792ce77c95cc61c805c0b915d5b8ffc4
Santiago Ocon <ocon.santiago@gmail.com>
2022-05-31 06:52:37 -0300

    Minor refactor.

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
-import CardActionArea from '@mui/material/CardActionArea'
 import CardMedia from '@mui/material/CardMedia'
 import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import { useDispatch } from 'react-redux'
-import { openEditModal, openDetailsModal } from '../../slices/modal'
+import { openEditModal } from '../../slices/modal'
 import { deleteProduct, getProducts } from '../../slices/extraReducers'
 import { toast } from 'react-hot-toast'
 import noImg from '../../assets/no-product-image.jpg'
@@ -19,10 +18,6 @@ const ProductCard = ({ product }) => {
 
   const handleEditClick = () => {
     dispatch(openEditModal(product._id))
-  }
-
-  const handleDetailsClick = () => {
-    dispatch(openDetailsModal(product._id))
   }
 
   const handleDeleteClick = () => {
@@ -38,33 +33,31 @@ const ProductCard = ({ product }) => {
   return (
     <Card className='card'>
       <CardContent>
-        <CardActionArea onClick={handleDetailsClick}>
-          <Box className='card__container-top'>
-            <CardMedia
-              component='img'
-              alt='product'
-              className='card__image'
-              height='200'
-              image={product.imageUrl}
-              onError={(e) => {
-                e.target.src = noImg
-              }}
-            />
-          </Box>
-          <Box className='card__container-bottom'>
-            <Typography gutterBottom variant='h5' component='div'>
-              {product.name}
-            </Typography>
-            <Typography
-              gutterBottom
-              variant='h6'
-              component='div'
-            >{`$ ${product.price}`}</Typography>
-          </Box>
-          <Typography variant='body2' color='text.secondary'>
-            {product.description}
+        <Box className='card__container-top'>
+          <CardMedia
+            component='img'
+            alt='product'
+            className='card__image'
+            height='200'
+            image={product.imageUrl}
+            onError={(e) => {
+              e.target.src = noImg
+            }}
+          />
+        </Box>
+        <Box className='card__container-bottom'>
+          <Typography gutterBottom variant='h5' component='div'>
+            {product.name}
           </Typography>
-        </CardActionArea>
+          <Typography
+            gutterBottom
+            variant='h6'
+            component='div'
+          >{`$ ${product.price}`}</Typography>
+        </Box>
+        <Typography variant='body2' color='text.secondary'>
+          {product.description}
+        </Typography>
       </CardContent>
       <CardActions>
         <Box className='card__actions'>
